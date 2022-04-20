9630412baaaad4be2155d62c87726d337d11a49b
LAPTOP-C6ITH8JV\정지원 <dhktjr0204442@gmail.com>
2022-04-20 21:05:03 +0900

    최솟값구하기

---
+++ p24/num24.java
@@ -0,0 +1,34 @@
+package p24;
+
+import java.util.Scanner;
+
+public class num24 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int N=sc.nextInt();
+
+        long[] dist=new long[N-1];
+        long[] cost=new long[N];
+
+        for(int i=0;i<N-1;i++){
+            dist[i]= sc.nextLong();
+        }
+
+        for(int i=0;i<N;i++){
+            cost[i]=sc.nextLong();
+        }
+
+        int sum=0;
+        long minCost=cost[0];
+
+        for(int i=0;i<N;i++){
+            if(cost[i]<minCost){
+                minCost=cost[i];
+            }
+
+            sum+=(minCost*dist[i]);
+        }
+        System.out.println(sum);
+    }
+}
