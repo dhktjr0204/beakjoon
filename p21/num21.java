bbd23521afc8453cbc661085624f0e8c20eb9f49
LAPTOP-C6ITH8JV\정지원 <dhktjr0204442@gmail.com>
2022-04-28 21:32:53 +0900

    최댓값 찾기

---
+++ p21/num21.java
@@ -0,0 +1,43 @@
+package p21;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class num21 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int[][] arr;
+        arr=new int[N][2];
+
+        for(int i=0;i<N;i++){
+            arr[i][0]= sc.nextInt();
+            arr[i][1]= sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]==o2[1]){
+                    return o1[0]-o2[0];
+                }
+                return o1[1]-o1[1];
+            }
+        });
+
+        int count=0;
+        int prev_end_time=0;
+
+        for(int i=0;i<N;i++){
+            if(prev_end_time<=arr[i][0]){
+                prev_end_time=arr[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+
+    }
+}
