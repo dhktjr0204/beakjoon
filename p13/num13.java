1369e61d34edf8ddb5e12591b90d56af32accc81
LAPTOP-C6ITH8JV\정지원 <dhktjr0204442@gmail.com>
2022-05-13 21:52:29 +0900

    1932번:삼각형 만들기

---
+++ p13/num13.java
@@ -0,0 +1,38 @@
+package p13;
+
+import java.util.Scanner;
+
+public class num13 {
+    static int[][] arr;
+    static Integer[][] dp;
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N=sc.nextInt();
+
+        arr=new int[N][N];
+        dp=new Integer[N][N];
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<i+1;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<N;i++){
+            dp[N-1][i]=arr[N-1][i];
+        }
+
+        System.out.println(find(0,0));
+    }
+    static int find(int depth,int idx){
+
+        if(depth==N-1) return dp[depth][idx];
+
+        if(dp[depth][idx]==null){
+            dp[depth][idx]=Math.max(dp[depth+1][idx],dp[depth+1][idx+1])+arr[depth][idx];
+        }
+        return dp[depth][idx];
+    }
+}
