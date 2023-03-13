24efc67a7e5cb3d52c815e481e13591cc134888e
dhktjr0204 <dhktjr0204442@gmail.com>
2023-03-13 15:49:47 +0900

    좌표 압축_Ver.2

---
+++ 정렬/18870_another_ver.py
@@ -0,0 +1,7 @@
+n=int(input())
+num_list=list(map(int, input().split()))
+sort_list=sorted(set(num_list))
+dic={sort_list[i] : i for i in range(len(sort_list))}
+
+for i in num_list:
+    print(dic[i],end=' ')
