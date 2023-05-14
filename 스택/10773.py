58008dadb275d555a8641211227590e88273d442
dhktjr0204 <dhktjr0204442@gmail.com>
2023-05-14 13:48:40 +0900

    제로

---
+++ 스택/10773.py
@@ -0,0 +1,13 @@
+import sys
+n=int(sys.stdin.readline())
+
+stack=[]
+for i in range(n):
+    m=int(sys.stdin.readline())
+
+    if m==0:
+        stack.pop()
+    else:
+        stack.append(m)
+    
+print(sum(stack))
