77ab52ed263f8dfecc11299802146fd740347459
dhktjr0204 <dhktjr0204442@gmail.com>
2023-05-21 22:23:10 +0900

    요세푸스 문제0

---
+++ 큐,덱/11866.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+n,k=map(int,sys.stdin.readline().split())
+q=deque([i for i in range(1,n+1)])
+
+print('<',end="")
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())
+    print(q.popleft(),end="")
+    if q:
+        print(', ',end="")
+
+print(">")
