2b695935e87671ecd91edc17b753c6059465a482
dhktjr0204 <dhktjr0204442@gmail.com>
2024-04-24 10:05:01 +0900

    수박수박수박수박수 - 자바

---
+++ 프로그래머스/lv2/수박수.java
@@ -0,0 +1,17 @@
+public class 수박수 {
+    public String solution(int n) {
+        String answer = "";
+        String subak="수박";
+        if (n%2==0){
+            answer= subak.repeat(n/2);
+        }else{
+            answer=subak.repeat(n/2)+"수";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        수박수 subak = new 수박수();
+        System.out.println(subak.solution(4));
+    }
+}
