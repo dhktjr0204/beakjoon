e5b8e1f73fbb772d764a5924febb9fb302577a6b
dhktjr0204 <dhktjr0204442@gmail.com>
2024-06-27 18:55:28 +0900

    1309번 - 동물원

---
+++ 동적계획법1/num1309.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class num1309 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] dp=new int[n][3];
+        dp[0][0]=1;
+        dp[0][1]=1;
+        dp[0][2]=1;
+        for (int i=1;i<n;i++){
+            dp[i][0]=(dp[i-1][0]+dp[i-1][1]+dp[i-1][2])%9901;
+            dp[i][1]=(dp[i-1][0]+dp[i-1][2])%9901;
+            dp[i][2]=(dp[i-1][0]+dp[i-1][1])%9901;
+        }
+        int answer=(dp[n-1][0]+dp[n-1][1]+dp[n-1][2])%9901;
+        System.out.println(answer);
+    }
+}
