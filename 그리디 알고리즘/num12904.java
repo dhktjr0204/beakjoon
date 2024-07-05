cbbba349bd497b4037475d9d9498d4c86bee9d74
dhktjr0204 <dhktjr0204442@gmail.com>
2024-07-05 20:16:52 +0900

    A와 B

---
+++ 그리디 알고리즘/num12904.java
@@ -0,0 +1,25 @@
+package gold;
+
+import java.io.*;
+
+public class num12904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer s=new StringBuffer(br.readLine());
+        StringBuffer t=new StringBuffer(br.readLine());
+
+        while (s.length()<t.length()){
+            if(t.charAt(t.length()-1)=='A'){
+                t.deleteCharAt(t.length()-1);
+            }else{
+                t.deleteCharAt(t.length()-1);
+                t.reverse();
+            }
+        }
+        if(s.toString().equals(t.toString())){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
