ba60c7aa1d0f6cab7b7ecafe2cf0e9342dc3135b
dhktjr0204 <dhktjr0204442@gmail.com>
2025-01-29 19:48:12 +0900

    가장 큰 수

---
+++ 프로그래머스/lv2/가장큰수.java
@@ -0,0 +1,21 @@
+package lv2;
+
+import java.util.*;
+
+public class 가장큰수 {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] arr= Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+
+        Arrays.sort(arr,(o1,o2)->(o2+o1).compareTo(o1+o2));
+
+        StringBuilder sb=new StringBuilder();
+        for(String s: arr){
+            sb.append(s);
+        }
+
+        answer=sb.toString();
+
+        return answer.charAt(0)=='0' ? "0": answer;
+    }
+}
