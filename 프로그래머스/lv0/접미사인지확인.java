6eff8a86f18b04807b1969109acef41a6a5b293b
dhktjr0204 <dhktjr0204442@gmail.com>
2025-03-27 13:09:18 +0900

    접미사인지 확인하기

---
+++ 프로그래머스/lv0/접미사인지확인.java
@@ -0,0 +1,13 @@
+package lv0;
+
+public class 접미사인지확인 {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+
+        if(my_string.endsWith(is_suffix)) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
