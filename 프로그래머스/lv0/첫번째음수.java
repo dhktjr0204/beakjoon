654090e25908542a8837fc9a4c5d64f0c2a2a5ea
dhktjr0204 <dhktjr0204442@gmail.com>
2025-04-11 07:56:46 +0900

    첫 번째로 나오는 음수

---
+++ 프로그래머스/lv0/첫번째음수.java
@@ -0,0 +1,15 @@
+package lv0;
+
+public class 첫번째음수 {
+    public int solution(int[] num_list) {
+        int answer = -1;
+
+        for(int i=0; i<num_list.length; i++) {
+            if(num_list[i] < 0) {
+                return i;
+            }
+        }
+
+        return answer;
+    }
+}
