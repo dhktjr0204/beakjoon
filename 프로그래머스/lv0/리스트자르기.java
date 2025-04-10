20718f2f976eef0a6087b870dc8fb53dce097c04
dhktjr0204 <dhktjr0204442@gmail.com>
2025-04-10 20:57:16 +0900

    리스트자르기

---
+++ 프로그래머스/lv0/리스트자르기.java
@@ -0,0 +1,30 @@
+package lv0;
+
+import java.util.*;
+
+public class 리스트자르기 {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int[] answer = {};
+
+        switch(n) {
+            case 1:
+                answer = Arrays.copyOfRange(num_list, 0, slicer[1] + 1);
+                break;
+            case 2:
+                answer = Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+                break;
+            case 3:
+                answer = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
+                break;
+            case 4:
+                List<Integer> list = new ArrayList<>();
+                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                    list.add(num_list[i]);
+                }
+                answer = list.stream().mapToInt(Integer::intValue).toArray();
+                break;
+        }
+
+        return answer;
+    }
+}
