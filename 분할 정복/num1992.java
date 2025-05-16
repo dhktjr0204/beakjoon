8dbe8c4d70140bfa22ed3fdcd66e70fa9b323e5d
JIwon Jeong <92018325+dhktjr0204@users.noreply.github.com>
2025-05-16 09:54:32 +0900

    쿼드트리

---
+++ 분할 정복/num1992.java
@@ -0,0 +1,54 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class num1992 {
+    static StringBuffer result = new StringBuffer();
+    static String[] matrix;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        matrix = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            matrix[i] = sc.next();
+        }
+
+        dfs(0, 0, n);
+
+        System.out.println(result.toString());
+    }
+
+    public static void dfs(int xStart, int yStart, int count) {
+        char startNum = matrix[yStart].charAt(xStart);
+
+        boolean isAllSame = true;
+        for (int i = yStart; i < yStart + count; i++) {
+            for (int j = xStart; j < xStart + count; j++) {
+                if (startNum != matrix[i].charAt(j)) {
+                    isAllSame = false;
+                    break;
+                }
+            }
+        }
+
+        if (isAllSame) {
+            result.append(startNum);
+            return;
+        }
+
+        result.append('(');
+
+        int newCount = count / 2;
+        dfs(xStart, yStart, newCount);
+        dfs(xStart + newCount, yStart, newCount);
+        dfs(xStart, yStart + newCount, newCount);
+        dfs(xStart + newCount, yStart + newCount, newCount);
+
+        result.append(')');
+    }
+}
